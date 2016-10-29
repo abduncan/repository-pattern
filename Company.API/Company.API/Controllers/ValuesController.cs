0e46d004e9348c8bf6875b88bddf82fd4ef00727
Andrew Duncan <abduncan0@gmail.com>
2016-10-28 20:01:41 -0400

    comments

--- Company.API/Company.API/Controllers/ValuesController.cs
+++ Company.API/Company.API/Controllers/ValuesController.cs
@@ -11,15 +11,21 @@ namespace Company.API.Controllers
 {
     public class ValuesController : ApiController
     {
+        /// <summary>
+        /// Hold our injected instance of the user repository.
+        /// </summary>
         private readonly IUserRepository _userRepository;
 
         public ValuesController(IUserRepository userRepository)
         {
+            // Store the injected user repository.
             _userRepository = userRepository;
         }
 
         public User Get(string email)
         {
+            // Use the concrete user repository to retrieve
+            // a user by email.
             return _userRepository.GetUserByEmail(email);
         }
     }
